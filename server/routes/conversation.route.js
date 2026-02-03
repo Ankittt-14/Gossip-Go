c920d43a88775900a6b81d5dd9660c375f6386de
Ankittt-14 <masterrajaniket@gmail.com>
2026-02-03 22:54:14 +0530

    fix: correct middleware import path in conversation routes

--- server/routes/conversation.route.js
+++ server/routes/conversation.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { protectRoute } from "../middlewares/protectRoute.middleware.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import { createGroup, getMyConversations } from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
 router.push = router.post; // Alias for consistency if needed, but standard is post
 
-router.post("/create-group", protectRoute, createGroup);
-router.get("/my-conversations", protectRoute, getMyConversations);
+router.post("/create-group", isAuthenticated, createGroup);
+router.get("/my-conversations", isAuthenticated, getMyConversations);
 
 export default router;
